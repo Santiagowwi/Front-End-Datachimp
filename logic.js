448b0eebdb3e5a3eb2078584ff7f3732b9b675b4
Santiagowwi <122487744+Santiagowwi@users.noreply.github.com>
2023-05-11 01:41:57 -0500

    Se arregla el LiviStream de las respuestas desde el backend

--- logic.js
+++ logic.js
@@ -1,15 +1,6 @@
 const msgerForm = get(".msger-inputarea");
 const msgerInput = get(".msger-input");
-const msgerInput_2 = get(".msger-input");
 const msgerChat = get(".msger-chat");
-
-const BOT_MSGS = [
-  "Hi, how are you?",
-  "Ohh... I can't understand what you trying to say. Sorry!",
-  "I like to play games... But I don't know how to play!",
-  "Sorry if my answers are not relevant. :))",
-  "I feel sleepy! :("
-];
 
 // Icons made by Freepik from www.flaticon.com
 const BOT_IMG = "https://marketing.sandbox.simetrik-beta.io/style-mvp/04-Texto-2/Bullet-blue-icon-4.png";
@@ -17,40 +8,43 @@ const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
 const BOT_NAME = "DataChimp MVP";
 const PERSON_NAME = "Santiago";
 
+let datachimpbot = ""; // variable declarada en un ámbito superior y inicializada vacía
 
-msgerForm.onsubmit=(event)=>{
-  event.preventDefault();  
-  fetch('https://i7w9qvsht1.execute-api.us-east-1.amazonaws.com/v1/invoke-lambda', {
-              method: "POST",
-              body: JSON.stringify({
-                message: msgerInput_2.value
-              }),
-              headers: {
-                  "Content-Type":"application/json"
-              }
-          })
-          .then( res => res.json())
-          .then( data => console.log(data))
-          console.log(msgerInput_2.value)
-  
-  }
-  
-
-
-
+fetch('https://i7w9qvsht1.execute-api.us-east-1.amazonaws.com/v1/invoke-lambda')
+  .then((response) => response.json())
+  .then((data) => {
+    datachimpbot = data.body; // actualizo el valor de la variable con la respuesta de la API
+    console.log(datachimpbot);
+  })
+  .catch((error) => console.log("No funciona la API"));
 
 msgerForm.addEventListener("submit", event => {
   event.preventDefault();
 
-  const msgText = msgerInput.value;
+  const msgText = msgerInput.value.trim();
   if (!msgText) return;
 
   appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
   msgerInput.value = "";
 
-  botResponse();
+  // envío el mensaje del usuario a la API para obtener la respuesta del bot
+  fetch('https://i7w9qvsht1.execute-api.us-east-1.amazonaws.com/v1/invoke-lambda', {
+    method: "POST",
+    body: JSON.stringify({
+      message: msgText
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(res => res.json())
+  .then(data => {
+    datachimpbot = data.body; // actualizo el valor de la variable con la respuesta de la API
+    console.log(datachimpbot);
+    appendMessage(BOT_NAME, BOT_IMG, "left", datachimpbot);
+  })
+  .catch((error) => console.log("No funciona la API"));
 });
-
 
 function appendMessage(name, img, side, text) {
   //   Simple solution for small apps
@@ -69,71 +63,9 @@ function appendMessage(name, img, side, text) {
     </div>
   `;
 
-
   msgerChat.insertAdjacentHTML("beforeend", msgHTML);
   msgerChat.scrollTop += 500;
 }
-
-
-
-
-
-let datachimpbot; // variable declarada en un ámbito superior
-let msgText;
-
-
-
-fetch('https://i7w9qvsht1.execute-api.us-east-1.amazonaws.com/v1/invoke-lambda')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    datachimpbot = data.body; // asigno el valor a la variable en el ámbito superior
-    console.log(datachimpbot);
-  })
-  .catch((error) => console.log("No funciona la API"));
-
-
-function botResponse() {
-  
-  const msgText = datachimpbot; // utilizo la variable en la función botResponse
-  
-
-  setTimeout(() => {
-    appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
-  }, 1);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Utils
 function get(selector, root = document) {
@@ -150,8 +82,3 @@ function formatDate(date) {
 function random(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-
-
-
-
-
